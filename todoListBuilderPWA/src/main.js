7009d0d5b2d4f766c6909ce9e9c245c43de6383a
ivolga <olga.v.ivanchenko@gmail.com>
2017-08-01 19:11:40 +0300

    fix root folder

--- todoListBuilderPWA/src/main.js
+++ todoListBuilderPWA/src/main.js
@@ -3,7 +3,7 @@ import ToDoBuilder from "./todobuilder.js";
 var todoBuilder = new ToDoBuilder( ".js-todo-builder" );
 
 if( "serviceWorker" in navigator ) {
-  navigator.serviceWorker.register( "/todoListBuilderPWA/sw.js" ) // all domain is the scope todoListBuilderPWA
+  navigator.serviceWorker.register( "/js-assignments/todoListBuilderPWA/sw.js" ) // all domain is the scope todoListBuilderPWA
   .then( ( registration ) => {
     console.log( "sw registration was done!",  registration );
   });
