c30123a594fc1f2d5d27c219fb53c674da0d6520
ivolga <olga.v.ivanchenko@gmail.com>
2017-08-01 18:44:22 +0300

    add PWA features: manifest.json and serviceWorker

--- todoListBuilderPWA/src/main.js
+++ todoListBuilderPWA/src/main.js
@@ -1,3 +1,10 @@
 import ToDoBuilder from "./todobuilder.js";
 
 var todoBuilder = new ToDoBuilder( ".js-todo-builder" );
+
+if( "serviceWorker" in navigator ) {
+  navigator.serviceWorker.register( "/sw.js" ) // all domain is the scope
+  .then( ( registration ) => {
+    console.log( "sw registration was done!",  registration );
+  });
+}
---
+++ todoListBuilderPWA/sw.js
@@ -0,0 +1,39 @@
+let cacheName = "todoPWA";
+let filesToCache = [
+  "/",
+  "/index.html",
+  "/dist/bundle.js",
+  "/fonts.css",
+  "/style.css",
+  "https://fonts.gstatic.com/s/roboto/v16/d-6IYplOFocCacKzxwXSOJBw1xU1rKptJj_0jans920.woff2",
+  "https://fonts.gstatic.com/s/roboto/v16/isZ-wbCXNKAbnjo6_TwHToX0hVgzZQUfRDuZrPvH3D8.woff2"
+];
+
+self.addEventListener( "install", ( ev ) => {
+  ev.waitUntil(
+    caches.open( cacheName ).then(( cache ) => {
+      return cache.addAll( filesToCache );
+    })
+  );
+});
+
+self.addEventListener( "activate", ( ev ) => {
+  ev.waitUntil(
+    caches.keys().then( ( keyList ) => {
+      return Promise.all( keyList.map( ( key ) => {
+        if( key !== cacheName  ) {
+          return caches.delete( key );
+        }
+      }))
+    })
+  );
+});
+
+self.addEventListener( "fetch", ( ev ) => {
+  ev.respondWith(
+    caches.match( ev.request ).then( ( response ) => {
+      console.log( "fetch responce", !!response );
+      return response || fetch( ev.request );
+    })
+  );
+});
